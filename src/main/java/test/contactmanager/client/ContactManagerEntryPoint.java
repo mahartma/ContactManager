d4a4764c45bb71e66e456c6ff5942e40cfcab1c5
max.hartmann <max.hartmann@codecentric.de>
2012-11-08 23:46:03 +0100

    comment for ContactManagerEntryPoint
    Change-Id: I82d49b4ed63c454cb2526fe528c684ddfe08d7d5

--- src/main/java/test/contactmanager/client/ContactManagerEntryPoint.java
+++ src/main/java/test/contactmanager/client/ContactManagerEntryPoint.java
@@ -4,6 +4,12 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
+/**
+ * this starts the gwt-application
+ * 
+ * @author max.hartmann
+ *
+ */
 public class ContactManagerEntryPoint implements EntryPoint {
 
 	public void onModuleLoad() {
